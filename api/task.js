17e67401468be3dd5cde320b28fb40682ccfe79f
Kayo Riccelo <kayo.riccelo@hotmail.com>
2019-09-18 16:57:45 -0300

    created update task; added route tasks;

--- api/task.js
+++ api/task.js
@@ -33,16 +33,42 @@ module.exports = app => {
                 if (rowsDeleted > 0) {
                     res.status(204).send();
                 } else {
-                    const msg = `Task not found with id ${req.params.id}.`;
+                    const msg = `Task with id ${req.params.id} not found.`;
                     res.status(400).send(msg);
                 };
             })
             .catch(err => res.status(400).json(err));
     };
 
+    const updateTaksDoneAt = (req, res) => {
+        app.db('tasks')
+            .where({ id: req.params.id, userId: req.user.id })
+            .update({ doneAt })
+            .then(_ => res.status(204).send())
+            .catch(err => res.status(400).json(err));
+    };
+
+    const toggleTask = (req, res) => {
+        app.db('tasks')
+            .where({ id: req.params.id, userId: req.user.id })
+            .first()
+            .then(task => {
+                if (!task) {
+                    const msg = `Task with id ${req.params.id} not found.`;
+                    return res.status(400).send(mgs);
+                };
+
+                const doneAt = task.doneAt ? null : new Date();
+
+                updateTaksDoneAt(req, res, doneAt);
+            })
+            .catch(err => res.status(400).json(err));
+    };
+
     return {
         getTasks,
+        toggleTask,
         save,
         remove
-    }
+    };
 };
